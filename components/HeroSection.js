64e5e5679344787e880a7d2725b20e959464f26f
DanielPerlman <dan.perlman.prog@gmail.com>
2020-03-18 20:36:11 +0100

    Changed hero section title to be more descriptive

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -2,7 +2,7 @@
 const HeroSection = () => (
   <div className="hero-section">
     <h1 className="title">
-      Welcome to Corona delivery
+      Get food delivered right to your doorstep and help at the same time
     </h1>
     <style jsx>{`
       .hero-section {
@@ -10,7 +10,7 @@ const HeroSection = () => (
         height: 10vh;
         background: #009688;
       }
-      
+
       .title {
         text-align: center;
         color: white;
