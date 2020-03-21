7e3b9aaf6add5e93d7840f03f709fe0e1a37b53b
DanielPerlman <dan.perlman.prog@gmail.com>
2020-03-21 17:26:44 +0100

    Fixed config for production build

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const withCSS = require("@zeit/next-css");
 const withSass = require("@zeit/next-sass");
 const withMDX = require("@zeit/next-mdx")();
 const path = require('path');
+const resolve = require('resolve')
 
 // https://spectrum.chat/thread/ba3668b1-f0b1-42a6-9c71-d7d9d3b67f04
 if (typeof require !== "undefined") {
@@ -11,7 +12,10 @@ if (typeof require !== "undefined") {
 
 const _cfg = (cfg, extra) => Object.assign(cfg || {}, extra);
 
-const _css = cfg => withCSS(_cfg(cfg, {}));
+const _css = cfg => withCSS(_cfg(cfg, {
+  cssModules: true,
+
+}));
 const _sass = cfg => withSass(_cfg(cfg));
 const _mdx = cfg =>
   withMDX(
@@ -22,6 +26,36 @@ const _mdx = cfg =>
           test: /\.ya?ml$/,
           use: "js-yaml-loader"
         });
+        const { dir, isServer } = options
+
+        config.externals = []
+
+        if (isServer) {
+          config.externals.push((context, request, callback) => {
+            resolve(request, { basedir: dir, preserveSymlinks: true }, (err, res) => {
+              if (err) {
+                return callback()
+              }
+
+              // Next.js by default adds every module from node_modules to
+              // externals on the server build. This brings some undesirable
+              // behaviors because we can't use modules that require CSS files like
+              // `former-kit-skin-pagarme`.
+              //
+              // The lines below blacklist webpack itself (that cannot be put on
+              // externals) and `former-kit-skin-pagarme`.
+              if (
+                res.match(/node_modules[/\\].*\.js/)
+                && !res.match(/node_modules[/\\]webpack/)
+                && !res.match(/node_modules[/\\]react-flippy/)
+              ) {
+                return callback(null, `commonjs ${request}`)
+              }
+
+              callback()
+            })
+          })
+        }
         return config;
       }
     })
